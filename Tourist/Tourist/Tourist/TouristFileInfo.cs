da8c00f37dfe9e0d3f6909386ecb6ccee572b60f
namegpark <namegpark@gmail.com>
2017-04-15 14:57:28 +0900

    Tourist 기반작업
    - 리스트뷰 컨트롤 메소드 추가
    - 기본파일정보 가져오는 클래스 및 프로퍼티 추가

---
+++ Tourist/Tourist/Tourist/TouristFileInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Tourist.Tourist
+{
+    public static class TouristFileInfo
+    {
+
+        // 해당파일의 기본정보를 가져오는 메소드
+        public static PropertyFileInfo getFileInfo(string filePath)
+        {
+            PropertyFileInfo pf = new PropertyFileInfo();
+            pf.filename = Path.GetFileName(filePath);
+
+            FileInfo fi = new FileInfo(filePath);
+            pf.filesize = fi.Length;
+            return pf;
+        }
+        
+    }
+
+    public class PropertyFileInfo
+    {
+        public string filename { get; set; }
+        public long filesize { get; set; }
+
+    }
+}
